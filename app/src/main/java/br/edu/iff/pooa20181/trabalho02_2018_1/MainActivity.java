4fe149ed14ade56358a4dfa9cd2b924a6351696f
matheusma37 <mtsma37@gmail.com>
2018-06-25 21:38:56 -0300

    Criação de um botão de confirmação para o menu da activity principal;

--- app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/MainActivity.java
@@ -1,13 +1,35 @@
 package br.edu.iff.pooa20181.trabalho02_2018_1;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerMenu);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.array_menu,
+                R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Button btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
+        btnConfirmar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                int opcao = spinner.getSelectedItemPosition();
+                if(opcao == 0){
+
+                }else if(opcao == 1){
+
+                }
+            }
+        });
     }
 }
