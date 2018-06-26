77add0916fba072266d2c9fe30363f84ac37420e
matheusma37 <mtsma37@gmail.com>
2018-06-26 17:21:48 -0300

    Criação do model Candidato;

---
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/model/Candidato.java
@@ -0,0 +1,75 @@
+package br.edu.iff.pooa20181.trabalho02_2018_1.model;
+
+public class Candidato {
+    private String nome, partido, cargo, numeroNaUrna, estado, municipio;
+    private long numeroDeVotos;
+
+    public Candidato(String nome, String partido,
+                     String cargo, String numeroNaUrna,
+                     String estado, String municipio,
+                     long numeroDeVotos) {
+        this.nome = nome;
+        this.partido = partido;
+        this.cargo = cargo;
+        this.numeroNaUrna = numeroNaUrna;
+        this.estado = estado;
+        this.municipio = municipio;
+        this.numeroDeVotos = numeroDeVotos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getNumeroNaUrna() {
+        return numeroNaUrna;
+    }
+
+    public void setNumeroNaUrna(String numeroNaUrna) {
+        this.numeroNaUrna = numeroNaUrna;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public long getNumeroDeVotos() {
+        return numeroDeVotos;
+    }
+
+    public void setNumeroDeVotos(long numeroDeVotos) {
+        this.numeroDeVotos = numeroDeVotos;
+    }
+}
