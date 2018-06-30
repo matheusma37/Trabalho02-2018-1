9933ce79bb5e5fe38161339c2c6b3c6267fb1173
matheusma37 <mtsma37@gmail.com>
2018-06-30 15:12:11 -0300

    Mudando a classe EleitorActivity para trabalhar com o RecyclerView;

--- app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/activity/EleitorActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/activity/EleitorActivity.java
@@ -1,15 +1,69 @@
 package br.edu.iff.pooa20181.trabalho02_2018_1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+
+import java.util.List;
 
 import br.edu.iff.pooa20181.trabalho02_2018_1.R;
+import br.edu.iff.pooa20181.trabalho02_2018_1.adapter.ClickRecyclerViewListener;
+import br.edu.iff.pooa20181.trabalho02_2018_1.adapter.EleitorAdapter;
+import br.edu.iff.pooa20181.trabalho02_2018_1.model.Eleitor;
+import io.realm.Realm;
 
-public class EleitorActivity extends AppCompatActivity {
+public class EleitorActivity extends AppCompatActivity implements ClickRecyclerViewListener {
+
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eleitor);
+
+        Button btnEleitor = (Button) findViewById(R.id.buttonEleitor);
+
+        realm = Realm.getDefaultInstance();
+
+        btnEleitor.setOnClickListener((view) -> {
+            Intent intent = new Intent(EleitorActivity.this, Detalhe.class);
+            startActivity(intent);
+        });
+
+    }
+
+
+    protected void onResume() {
+
+        super.onResume();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewEleitor);
+
+        recyclerView.setAdapter(new EleitorAdapter(getEleitores(),this,this));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    public List<Eleitor> getEleitores(){
+
+        return (List) realm.where(Eleitor.class).findAll();
+
+    }
+
+    @Override
+    public void onClick(Object object) {
+        Eleitor eleitor = (Eleitor) object;
+        Intent intent = new Intent(EleitorActivity.this, Detalhe.class);
+        intent.putExtra("numeroDoTitulo",eleitor.getNumeroDoTitulo());
+        startActivity(intent);
+    }
+
+
+    public void finish(){
+        super.finish();
+        realm.close();
     }
 }
