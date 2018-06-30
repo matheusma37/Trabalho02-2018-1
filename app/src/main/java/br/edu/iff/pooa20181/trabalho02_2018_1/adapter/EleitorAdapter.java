0fc28f45ced435314b93b859128d521e1f7ef4fc
matheusma37 <mtsma37@gmail.com>
2018-06-30 05:20:46 -0300

    Mudando o adapter Eleitor para extender RecyclerView.Adapter;

--- app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/adapter/EleitorAdapter.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/adapter/EleitorAdapter.java
@@ -1,44 +1,60 @@
 package br.edu.iff.pooa20181.trabalho02_2018_1.adapter;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import br.edu.iff.pooa20181.trabalho02_2018_1.R;
 import br.edu.iff.pooa20181.trabalho02_2018_1.model.Eleitor;
 
-public class EleitorAdapter extends ArrayAdapter<Eleitor> {
+public class EleitorAdapter extends RecyclerView.Adapter<EleitorAdapter.EleitorViewHolder> {
 
-    private final Context context;
-    private final ArrayList<Eleitor> eleitores;
+    private List<Eleitor> eleitores;
+    private Context context;
+    private static ClickRecyclerViewListener clickRecyclerViewListener;
+    private RecyclerView.ViewHolder holder;
+    private int position;
 
-    public EleitorAdapter(Context context, ArrayList<Eleitor> eleitores) {
-        super(context, R.layout.linha_eleitor, eleitores);
-        this.context = context;
-        this.eleitores = eleitores;
+    @Override
+    public EleitorAdapter.EleitorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.linha_candidato, parent, false);
+        EleitorAdapter.EleitorViewHolder eleitorViewHolder = new EleitorAdapter.EleitorViewHolder(view);
+        return eleitorViewHolder;
     }
 
     @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+    public void onBindViewHolder(EleitorAdapter.EleitorViewHolder viewHolder, int position) {
+        EleitorAdapter.EleitorViewHolder eleitorViewHolder = viewHolder;
+        Eleitor eleitor = this.eleitores.get(position);
+        eleitorViewHolder.nome.setText(eleitor.getNome());
+        eleitorViewHolder.titulo.setText(eleitor.getNumeroDoTitulo());
+        eleitorViewHolder.secao.setText(eleitor.getSecao());
+        eleitorViewHolder.zona.setText(eleitor.getZona());
+    }
 
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    @Override
+    public int getItemCount() {
+        return eleitores.size();
+    }
 
-        View rowView = inflater.inflate(R.layout.linha_eleitor, parent, false);
-        TextView nome = (TextView) rowView.findViewById(R.id.nomeEleitor);
-        TextView titulo = (TextView) rowView.findViewById(R.id.numeroTituloEleitor);
-        TextView secao = (TextView) rowView.findViewById(R.id.secaoEleitor);
-        TextView zona = (TextView) rowView.findViewById(R.id.zonaEleitor);
-        nome.setText(eleitores.get(position).getNome());
-        titulo.setText(eleitores.get(position).getNumeroDoTitulo());
-        secao.setText(eleitores.get(position).getSecao());
-        zona.setText(eleitores.get(position).getZona());
-        return rowView;
+    public static class EleitorViewHolder extends RecyclerView.ViewHolder{
+        private final TextView nome;
+        private final TextView titulo;
+        private final TextView secao;
+        private final TextView zona;
+
+        public EleitorViewHolder(View view){
+            super(view);
+            nome = (TextView) itemView.findViewById(R.id.nomeEleitor);
+            titulo = (TextView) itemView.findViewById(R.id.numeroTituloEleitor);
+            secao = (TextView) itemView.findViewById(R.id.secaoEleitor);
+            zona = (TextView) itemView.findViewById(R.id.zonaEleitor);
+        }
     }
 }
